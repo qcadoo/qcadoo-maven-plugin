07a2f42bfada42ac62d73ebd5b73a0ab3056162d
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2011-05-16 10:51:28 +0200

    RF# move maven plugin into separate repository

---
+++ src/main/java/com/qcadoo/maven/plugins/validator/ValidationErrorHandler.java
@@ -0,0 +1,24 @@
+package com.qcadoo.maven.plugins.validator;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class ValidationErrorHandler implements ErrorHandler {
+
+    @Override
+    public void error(final SAXParseException e) throws SAXException {
+        throw new SAXException(e);
+    }
+
+    @Override
+    public void fatalError(final SAXParseException e) throws SAXException {
+        throw new SAXException(e);
+    }
+
+    @Override
+    public void warning(final SAXParseException e) throws SAXException {
+        throw new SAXException(e);
+    }
+
+}
