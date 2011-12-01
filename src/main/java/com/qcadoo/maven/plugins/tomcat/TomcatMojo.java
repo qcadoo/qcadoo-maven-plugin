1c4534f29366b05bbaf63b26d38cfde36d03d701
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2011-12-01 15:02:04 +0100

    RF# add commercial profile

--- src/main/java/com/qcadoo/maven/plugins/tomcat/TomcatMojo.java
+++ src/main/java/com/qcadoo/maven/plugins/tomcat/TomcatMojo.java
@@ -102,12 +102,6 @@ public class TomcatMojo extends AbstractMojo {
     private File libDirectory;
 
     /**
-     * @parameter expression= "${basedir}/target/tomcat-archiver/${project.artifactId}/webapps/ROOT/WEB-INF/lib/"
-     * @readonly
-     */
-    private File webappLibDirectory;
-
-    /**
      * @parameter expression= "${basedir}/target/tomcat-archiver/${project.artifactId}/bin/"
      * @readonly
      */
@@ -231,24 +225,6 @@ public class TomcatMojo extends AbstractMojo {
         return false;
     }
 
-    @SuppressWarnings("unchecked")
-    private boolean isSaasTomcatDeployApplicationProfileActive() {
-        boolean tomcat = false;
-        boolean saas = false;
-        boolean deployApplication = false;
-
-        for (Profile profile : ((List<Profile>) project.getActiveProfiles())) {
-            if ("tomcat".equals(profile.getId())) {
-                tomcat = true;
-            } else if ("saas".equals(profile.getId())) {
-                saas = true;
-            } else if ("deployApplication".equals(profile.getId())) {
-                deployApplication = true;
-            }
-        }
-        return tomcat && saas && deployApplication;
-    }
-
     private void registerArtifact() {
         project.getArtifact().setFile(target);
     }
@@ -311,16 +287,6 @@ public class TomcatMojo extends AbstractMojo {
         copyDependency(libDirectory, "org.apache.tomcat", "servlet-api", "6.0.29", "jar");
         copyDependency(libDirectory, "org.apache.tomcat", "coyote", "6.0.29", "jar");
         copyDependency(libDirectory, "org.apache.tomcat", "dbcp", "6.0.29", "jar");
-
-        addCommercialPluginsIfSaasDemo();
-    }
-
-    private void addCommercialPluginsIfSaasDemo() throws ArtifactResolutionException, ArtifactNotFoundException, IOException {
-        if (isSaasTomcatDeployApplicationProfileActive()) {
-            copyDependency(webappLibDirectory, "com.qcadoo.mes", "mes-commercial-plugins-integration-commons", "1.0.0", "jar");
-            copyDependency(webappLibDirectory, "com.qcadoo.mes", "mes-commercial-plugins-integration-enova", "1.0.0", "jar");
-            copyDependency(webappLibDirectory, "com.qcadoo.mes", "mes-commercial-plugins-integration-subiekt", "1.0.0", "jar");
-        }
     }
 
     private void copyClassPathResources() throws IOException {
